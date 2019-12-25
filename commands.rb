3efae8b895c20ad980a01f3a43f39bf6e4968385
Igor Voytov <selamba@ya.ru>
2019-12-25 20:09:13 +0300

    delete fully implemented; vastly improved structure of app

--- commands.rb
+++ commands.rb
@@ -1,32 +1,31 @@
-module Train
-    Create = ->(input, input_words) {
-        
-    }
-    Select = ->(input, input_words) {
-        if input_words[2] == "count"
-            command = "select count(*) from Trains"
-	
-            for str in input_words[3..-1]
-                command << " " << str
-            end
-    
-            App::DB.execute command do |row|
-                App::SHELL.msg("Кол-во поездов: #{row}")
-            end
-        elsif input_words[2] == "where"
-            command = "select * from Trains"
-	
-            for str in input_words[2..-1]
-                command << " " << str
-            end
-    
-            App::DB.execute command do |row|
-                App::SHELL.msg("#{row}")
-            end
+module Commands
+	def self.select(input_words)
+		table = entity(input_words[0])
+		
+		if input_words[2] == "count"
+			command = "select count(*) from #{table}"
+			
+			for str in input_words[3..-1]
+				command << " " << str
+			end
+			
+			App::DB.execute command do |row|
+				App::SHELL.msg("Кол-во сущностей: #{row}")
+			end
+		elsif input_words[2] == "where"
+			command = "select * from #{table}"
+			
+			for str in input_words[2..-1]
+				command << " " << str
+			end
+			
+			App::DB.execute command do |row|
+				App::SHELL.msg("#{row}")
+			end
 		elsif input_words[2].start_with?("column")
 			command = "select"
 			where_index = 3
-	
+			
 			for str in input_words[3..-1]
 				if str == "where"
 					break
@@ -34,19 +33,21 @@ module Train
 				where_index += 1
 				command << " " << str
 			end
-			command << " from Trains"
+			command << " from #{table}"
 			
 			for str in input_words[where_index..-1]
 				command << " " << str
 			end
-	
+			
 			App::DB.execute command do |row|
 				App::SHELL.msg("#{row}")
 			end
 		end
-    }
-    Update = ->(input, input_words) {
-		command = "update Trains set"
+	end
+	
+	def self.update(input_words)
+		table = entity(input_words[0])
+		command = "update #{table} set"
 		where_index = 3
 		
 		for str in input_words[3..-1]
@@ -56,166 +57,30 @@ module Train
 			where_index += 1
 			command << " " << str
 		end
-	
-		for str in input_words[where_index..-1]
-			command << " " << str
-		end
-	
-		App::DB.execute command
-		App::SHELL.msg("Изменение данных прошло успешно.")
-    }
-    Delete = ->(input, input_words) {
-        
-    }
-    Generate = ->(input, input_words) {
-        
-    }
-end
-
-module Station
-    Create = ->(input, input_words) {
-        
-        }
-    Select = ->(input, input_words) {
-        if input_words[2] == "count"
-            command = "select count(*) from Stations"
-	
-            for str in input_words[3..-1]
-                command << " " << str
-            end
-    
-            App::DB.execute command do |row|
-                App::SHELL.msg("Кол-во станций: #{row}")
-            end
-        elsif input_words[2] == "where"
-            command = "select * from Stations"
-	
-            for str in input_words[2..-1]
-                command << " " << str
-            end
-    
-            App::DB.execute command do |row|
-                App::SHELL.msg("#{row}")
-            end
-		elsif input_words[2].start_with?("column")
-			command = "select"
-			where_index = 3
-	
-			for str in input_words[3..-1]
-				if str == "where"
-					break
-				end
-				where_index += 1
-				command << " " << str
-			end
-			command << " from Stations"
-			
-			for str in input_words[where_index..-1]
-				command << " " << str
-			end
-	
-			App::DB.execute command do |row|
-				App::SHELL.msg("#{row}")
-			end
-		end
-    }
-    Update = ->(input, input_words) {
-		command = "update Stations set"
-		where_index = 3
 		
-		for str in input_words[3..-1]
-			if str == "where"
-				break
-			end
-			where_index += 1
-			command << " " << str
-		end
-	
 		for str in input_words[where_index..-1]
 			command << " " << str
 		end
-	
-		App::DB.execute command
-		App::SHELL.msg("Изменение данных прошло успешно.")
-        }
-    Delete = ->(input, input_words) {
-        
-        }
-    Generate = ->(input, input_words) {
-        
-        }
-end
-
-module Wagon
-    Create = ->(input, input_words) {
-        
-        }
-    Select = ->(input, input_words) {
-        if input_words[2] == "count"
-            command = "select count(*) from Wagons"
-	
-            for str in input_words[3..-1]
-                command << " " << str
-            end
-    
-            App::DB.execute command do |row|
-                App::SHELL.msg("Кол-во вагонов: #{row}")
-            end
-        elsif input_words[2] == "where"
-            command = "select * from Wagons"
-	
-            for str in input_words[2..-1]
-                command << " " << str
-            end
-    
-            App::DB.execute command do |row|
-                App::SHELL.msg("#{row}")
-            end
-		elsif input_words[2].start_with?("column")
-			command = "select"
-			where_index = 3
-	
-			for str in input_words[3..-1]
-				if str == "where"
-					break
-				end
-				where_index += 1
-				command << " " << str
-			end
-			command << " from Wagons"
-			
-			for str in input_words[where_index..-1]
-				command << " " << str
-			end
-	
-			App::DB.execute command do |row|
-				App::SHELL.msg("#{row}")
-			end
-		end
-    }
-    Update = ->(input, input_words) {
-		command = "update Wagons set"
-		where_index = 3
 		
-		for str in input_words[3..-1]
-			if str == "where"
-				break
-			end
-			where_index += 1
-			command << " " << str
-		end
-	
-		for str in input_words[where_index..-1]
-			command << " " << str
-		end
-	
 		App::DB.execute command
 		App::SHELL.msg("Изменение данных прошло успешно.")
-        }
-    Delete = ->(input, input_words) {
-        
-        }
-    Generate = ->(input, input_words) {
-        
-        }
+	end
+	
+	def self.delete(input_words)
+		table = entity(input_words[0])
+		command = "delete from #{table}"
+		
+		for str in input_words[2..-1]
+			command << " " << str
+		end
+		
+		App::DB.execute command
+		App::SHELL.msg("Удаление данных прошло успешно.")
+	end
+	
+	private
+	
+	def self.entity(str)
+		return (str.capitalize << "s")
+	end
 end
--- shell.rb
+++ shell.rb
@@ -31,7 +31,6 @@ class Shell
             if @@command_history.length > 32
                 @@command_history.delete_at(0)
             end
-            
 
             # Обработка ввода
             if input == "exit"
@@ -39,21 +38,7 @@ class Shell
                 return
             end
 			input_words = input.split(" ")
-            
-            # Обработка целевой таблицы
-            target_table = ""
-            case input_words[0]
-            when "train"
-                target_table = "Trains"
-            when "station"
-                target_table = "Stations"
-            when "wagon"
-                target_table = "Wagons"
-            else
-                self.msg("Неверная сущность: #{input_words[0]}")
-                next
-            end
-            
+                        
             # Обработка целевой команды
             target_command = input_words[1]
             if !Entity_commands.include?(target_command)
@@ -62,46 +47,7 @@ class Shell
             end
             
             # Самое интересное
-            if target_table == "Trains"
-                case target_command
-                when "create"
-                    Train::Create.call input, input_words
-                when "select"
-                    Train::Select.call input, input_words
-                when "update"
-                    Train::Update.call input, input_words
-                when "delete"
-                    Train::Delete.call input, input_words
-                when "generate"
-                    Train::Generate.call input, input_words
-                end
-            elsif target_table == "Stations"
-                case target_command
-                when "create"
-                    Station::Create.call input, input_words
-                when "select"
-                    Station::Select.call input, input_words
-                when "update"
-                    Station::Update.call input, input_words
-                when "delete"
-                    Station::Delete.call input, input_words
-                when "generate"
-                    Station::Generate.call input, input_words
-                end
-            elsif target_table == "Wagons"
-                case target_command
-                when "create"
-                    Wagon::Create.call input, input_words
-                when "select"
-                    Wagon::Select.call input, input_words
-                when "update"
-                    Wagon::Update.call input, input_words
-                when "delete"
-                    Wagon::Delete.call input, input_words
-                when "generate"
-                    Wagon::Generate.call input, input_words
-                end
-            end
+			Commands.send(target_command, input_words)
         end
     end
     
